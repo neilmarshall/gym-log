57a579f3fa62628654a969f49a5c237dc97af917
neilmarshall <neil.marshall@dunelm.org.uk>
2019-06-29 11:15:38 +0100

    update run.py to build a shell context

--- run.py
+++ run.py
@@ -1,7 +1,15 @@
 """
 Entry point for 'flask run' command
 """
-from app import create_app
-from config import Config
+from app import create_app, db
+from app.models.exercise import Exercise
+from app.models.gym_record import GymRecord
+from app.models.session import Session
+from app.models.user import User
 
-app = create_app(Config)
+app = create_app()
+
+@app.shell_context_processor
+def make_shell_context():
+    """Launch a Python interpreter pre-populated with an application context"""
+    return {'db': db, 'Exercise': Exercise, 'GymRecord': GymRecord, 'Session': Session, 'User': User}
