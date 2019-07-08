eb69009e7756addd336ff20b190c6b7b06825bea
Neil Marshall <Neil.Marshall@riskfirst.com>
2019-07-08 18:53:06 +0100

    add blueprint with common url prefix

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Blueprint, Flask
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
@@ -16,16 +16,20 @@ from app.resources.token import Token
 def create_app(config_object=Config):
     """Application Factory"""
     
+
     app = Flask(__name__)
     app.config.from_object(config_object)
 
     db.init_app(app)
     migrate.init_app(app, db)
 
-    api = Api(app)
-    api.add_resource(Exercises, '/api/exercises')
-    api.add_resource(Register, '/api/register')
-    api.add_resource(Sessions, '/api/sessions', '/api/sessions/<session_date>')
-    api.add_resource(Token, '/api/token')
+    bp = Blueprint('bp', __name__)
+    api = Api(bp)
+    api.add_resource(Exercises, '/exercises')
+    api.add_resource(Register, '/register')
+    api.add_resource(Sessions, '/sessions', '/sessions/<session_date>')
+    api.add_resource(Token, '/token')
+
+    app.register_blueprint(bp, url_prefix='/api')
 
     return app
